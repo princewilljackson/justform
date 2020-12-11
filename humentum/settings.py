231bf4493d76ec90ad8e7c5f90e305a5e9a9793f
princewilljackson <princewilljackson@ymail.com>
2020-12-11 08:51:23 +0100

    Debug equals False

--- humentum/settings.py
+++ humentum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eeii-cn-jp1^4uj!#1a*wiqm=mq!t71aa6ewtgf%sqdb6%mo^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
